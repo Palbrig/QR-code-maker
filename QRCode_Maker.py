4751e66602663483c5892639f99f72a339337dd6
Parker <102333250+Palbrig@users.noreply.github.com>
2026-09-22 14:37:26 -0500

    V 1.0.0

--- QRCode_Maker.py
+++ QRCode_Maker.py
@@ -1,8 +1,9 @@
 import sys
+from pathlib import Path
 import qrcode
 import qrcode.image.svg
 
-def create_qr(url, filename):
+def create_qr(url, filepath):
     factory = qrcode.image.svg.SvgPathImage
 
     img = qrcode.make(
@@ -10,14 +11,18 @@ def create_qr(url, filename):
         image_factory=factory
     )
 
-    img.save(filename)
+    img.save(filepath)
 
 def main():
     url = sys.argv[1]
     filename = sys.argv[2]
+    script_dir = Path(__file__).resolve().parent
+    folder = script_dir / "QR Codes"
+    folder.mkdir(exist_ok=True)
+    filepath = folder / (filename + ".svg")
 
-    create_qr(url, filename)
-    print(f"Saved QR code as {filename}")
+    create_qr(url, filepath)
+    print(f"Saved QR code as {filepath}")
 
 
 main()
